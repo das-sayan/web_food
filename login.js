80568d54e6f393a6e3ba72296bd99c945c0217e0
Sayan Das <34838531+das-sayan@users.noreply.github.com>
2020-01-17 21:12:09 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+
+		function admin(){
+			style.left = "-400px";
+			style.left = "50px";
+			style.left = "110px";
+		}
+
+		function login(){
+		style.left = "50px";
+			style.left = "450px";
+			style.left = "0";
+		}
